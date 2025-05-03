40512587da0bf677568b471327b5f9893e63830e
Srajan <5590sg@gmail.com>
2025-05-03 23:49:20 +0530

    Fix: make WebSocket event switch exhaustive

--- Sources/Channel.swift
+++ Sources/Channel.swift
@@ -311,7 +311,7 @@ public class Channel{
         self.reconnect()
     }
     
-     func onError(error: any Error){
+    public func onError(error: any Error){
         let event: PieSocketEvent = PieSocketEvent(event: "system:error");
         self.fireEvent(event: event);
     }
@@ -338,7 +338,7 @@ public class Channel{
         }
     }
     
-     func onMessage(text: String){
+     public func onMessage(text: String){
         
         var payload: PieSocketEvent = PieSocketEvent();
         
